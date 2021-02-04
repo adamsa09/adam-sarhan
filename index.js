9493a5a1b32e4b2bd3a90d693ae55424897a22c7
Adam Sarhan <adamsa09@gmail.com>
2021-02-04 17:50:48 -0500

    When user scrolls down it hides the top bar

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-// SOURCE: W3SCHOOLS
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
+// SOURCE: W3 SCHOOLS
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
     } else {
-        mybutton.style.display = "none";
+        document.getElementById("navbar").style.top = "-100px";
     }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-
-function hide(id) {
-    var item = document.getElementById(id);
-    item.style.visibility = "hidden";
+    prevScrollpos = currentScrollPos;
 }
